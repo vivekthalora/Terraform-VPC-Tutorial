9aaec39f6afded85df789e4873c7acb660dd169b
Vivek P V <vivekpv@riktech.co.in>
2019-05-01 10:32:35 +0530

    Removed AWS All Keys

--- provider.tf
+++ provider.tf
@@ -2,5 +2,8 @@
 provider "aws" {
   region = "${var.aws_region}"
   shared_credentials_file = "/root/.aws/credentials"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
   profile = "default"
 }
